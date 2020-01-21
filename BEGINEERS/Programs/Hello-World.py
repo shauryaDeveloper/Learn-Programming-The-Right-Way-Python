6b83b8db7b55ef761921efb18c4278807a7641a4
Shaurya Singh <45226713+shauryaDeveloper@users.noreply.github.com>
2020-01-21 19:31:40 +0530

    Hello-World.py

---
+++ BEGINEERS/Programs/Hello-World.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+
+#print is used to get an output
+
+#after that brackets and after brackets use single or double quotes
+
+#everything under the quotes will be printed
+
+a=3.0
+
+print(a)
+
+#if you have a pre-defined variable and want to print the value of that variable
+
+#all steps are same just the quotes are not used inside the brackets just the variable name
